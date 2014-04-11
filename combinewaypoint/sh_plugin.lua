c084f12370fa8b74ab0e2014212f6d6d637fe59a
fayzgolden666 <austinwoods59@gmail.com>
2014-04-10 23:03:32 -0600

    added proper porting for ns
    ported - has small error

--- combinewaypoint/sh_plugin.lua
+++ combinewaypoint/sh_plugin.lua
@@ -1,24 +1,16 @@
-PLUGIN.name = "Combine Waypoints"
-PLUGIN.author = "FayZ and Oedipus"
-PLUGIN.desc = "Adds waypoints for people in the combine faction."
-
-nut.util.Include("sv_hooks.lua")
-nut.util.Include("cl_hooks.lua")
-nut.util.Include("sh_config.lua")
-
 nut.command.Register({
-		onRun = function(player, arguments)
-			if (client:IsCombine()) then
-				local trace = client:GetEyeTraceNoCursor()
-				if (!trace.Hit) then return end
-				
-				local strings = {name = client:Name(), msg = table.concat(arguments, " ") or false}
-				local transmitstring = util.TableToJSON(strings)
-				net.Start("combinemarker")
-					net.WriteVector(trace.HitPos)
-					net.WriteString(transmitstring)
-				net.Broadcast()
-			else
+	onRun = function(client, arguments)
+		if (client:IsCombine()) then
+			local trace = client:GetEyeTraceNoCursor()
+			
+			if (!trace.Hit) then return end
+			 
+			local strings = {name = client:Name(), msg = table.concat(arguments, " ") or false}
+			local transmitstring = util.TableToJSON(strings)
+ 
+			netstream.Start(nil, "CombineMarker", {trace.HitPos,transmitstring})
+		else
 			nut.util.Notify("You're not apart of the Combine!", client)
 		end
+end
 }, "createwaypoint")
