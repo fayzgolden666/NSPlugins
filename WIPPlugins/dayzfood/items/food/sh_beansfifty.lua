0ff0ed38cfcd7a0cb92e5e4746843104257de2b3
fayzgolden666 <austinwoods59@gmail.com>
2014-04-14 16:43:55 -0600

    hotfix

--- WIPPlugins/dayzfood/items/food/sh_beansfifty.lua
+++ WIPPlugins/dayzfood/items/food/sh_beansfifty.lua
@@ -1,6 +1,6 @@
 ITEM.name = "Canned Beans [OPENED]"
 ITEM.uniqueID = "beans_food"
-ITEM.desc = "A can of beans that is %Amount|0% full ."
+ITEM.desc = "A can of beans that is %Amount|0% % full ."
 ITEM.model = Model("models/props_lab/jar01b.mdl")
 ITEM.restore = 50
 ITEM.data = {
