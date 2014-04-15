f61970b65f5e77e4ba801743ca58f213f2ebf9c0
Rubberbandits <exodusthefirst@gmail.com>
2014-04-14 18:01:27 -0700

    Fixed Bug

--- WIPPlugins/dayzfood/items/sh_beans.lua
+++ WIPPlugins/dayzfood/items/sh_beans.lua
@@ -11,7 +11,22 @@ ITEM.functions.Open = {
             icon = "icon16/error.png",
         run = function(itemTable, client, data)
             if (CLIENT) then return end
-     
+            
+            if !client:HasItem("kitchen_knife") then
+                nut.util.Notify("You don't have the tools to open the can!", client)
+                    return false
+            end
+            
+            if !client:HasItem("dayz_wrench") then
+                nut.util.Notify("You don't have the tools to open the can!", client)
+                    return false
+            end
+            
+            if !client:HasItem("dayz_stone") then
+                nut.util.Notify("You don't have the tools to open the can!", client)
+                    return false
+            end
+            
             if client:HasItem("kitchen_knife") then
                         client:UpdateInv("beans_food", 1, {Amount = math.random(40,50)})
                         client:EmitSound("physics/flesh/flesh_impact_hard"..math.random(1, 5)..".wav")
@@ -28,8 +43,6 @@ ITEM.functions.Open = {
                         client:UpdateInv("beans_food", 1, {Amount = math.random(10,20)})
                         client:EmitSound("physics/flesh/flesh_impact_hard"..math.random(1, 5)..".wav")
                         nut.util.Notify("You have opened the can of beans! A lot has spilled out.", client)                              
-                    else 
-                         nut.util.Notify("You don't have the required tools to open this can!", client)           
         end           
     end
 }
