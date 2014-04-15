9253f07ab3402fb1b80e66f7f25b3a1b327daa25
Rubberbandits <exodusthefirst@gmail.com>
2014-04-14 22:06:08 -0700

    Added comments; Fixed small bug

--- randomitems/sh_plugin.lua
+++ randomitems/sh_plugin.lua
@@ -1,28 +1,22 @@
-PLUGIN.name = "Spawn Items"
-PLUGIN.author = "Oepidus Rex and Sants1 (Fayz_Golden)"
-PLUGIN.desc = "Allows players to spawn with a set of random items"
-
------------------------------------------------------
-
 function PLUGIN:GetDefaultInv(inventory, client, data)
 	if (SERVER) then
 		if (data.faction == FACTION_CITIZEN) then
-
-			pistols = {"weapon_smg1", "weapon_shotgun", "weapon_pistol", "weapon_357"}
+			
+			pistols = {"weapon_smg1", "weapon_shotgun", "weapon_pistol", "weapon_357"} -- Edit this shit to your liking. These tables are meant for you to edit.
 			ammoitems = {"weapon_frag", "bleach", "water", "water"}
 			rarerifles = {"bleach", "water", "weapon_ar2"}
 			itssoflattering = {"bleach", "bleach", "bleach", "bleach", "bleach"}
-
+			
 			local addictionlikesAnal = table.Random(pistols)
 			local nicecopypastehaloKiller = table.Random(ammoitems)
-			local ateelovesDusty = math.random(1, 100)
+			local ateelovesDusty = math.random(1, 100) -- Just the chance modifier thing, leave this alone
 			local kipperisaFaggot = table.Random(rarerifles)
 			local thanksforcopypastingmycodehaloKiller = table.Random(itssoflattering)
-			local hollahollagetdolla = math.random(200, 300)
-			local yourepournigger = math.random(100, 150)
-
+			local hollahollagetdolla = math.random(200, 300) -- Money if number selected is over or equal to sixty
+			local yourepournigger = math.random(100, 150) -- This is the money for if the number selected is lower than sixty
+			
 -- please don't edit after this line my loves unless you want to get rekt (talking to you addiction) (you too halokiller) (you are buttbuddies and i know it)
-
+			
 			if (ateelovesDusty <= 60) then
 				client:UpdateInv(addictionlikesAnal)
 				client:UpdateInv(nicecopypastehaloKiller)
@@ -37,5 +31,5 @@ function PLUGIN:GetDefaultInv(inventory, client, data)
 end
 
 -- i need to make this all spaghetti code
-
 -- diabetic raptor if you are looking at this, fuck off loser
+-- halokiller, if your version of this is 7 lines then it seems like you clearly don't understand that the amount of lines doesn't fucking matter.
