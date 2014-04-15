74010abdf9362ccda15599f92f09356a8df7e2d7
Rubberbandits <exodusthefirst@gmail.com>
2014-04-15 16:57:40 -0700

    Update cl_hooks.lua

--- combinewaypoint/cl_hooks.lua
+++ combinewaypoint/cl_hooks.lua
@@ -2,6 +2,7 @@ local wptexture = surface.GetTextureID("combinemarker/marker")
 local client = LocalPlayer()
 
 netstream.Hook("combinemarker", function(data)
+	print("hello")
 	if client:IsCombine() then
 		local WaypointPos = data[1] + Vector(0,0,50)
 		local StringTable = util.JSONtoTable(data[2])
