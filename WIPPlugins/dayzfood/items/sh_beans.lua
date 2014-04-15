18c5e4b4b370165f63ee08c90243e17e2a76adf5
Rubberbandits <exodusthefirst@gmail.com>
2014-04-14 18:28:26 -0700

    Update sh_beans.lua

--- WIPPlugins/dayzfood/items/sh_beans.lua
+++ WIPPlugins/dayzfood/items/sh_beans.lua
@@ -14,13 +14,10 @@ ITEM.functions.Open = {
             
             if (!client:HasItem("kitchen_knife")) then
                 nut.util.Notify("You don't have the tools to open the can!", client)
-                    return false
             elseif (!client:HasItem("kitchen_knife")) then
                 nut.util.Notify("You don't have the tools to open the can!", client)
-                    return false
             elseif (!client:HasItem("kitchen_knife")) then
                 nut.util.Notify("You don't have the tools to open the can!", client)
-                    return false
             end
             
             if client:HasItem("kitchen_knife") then
