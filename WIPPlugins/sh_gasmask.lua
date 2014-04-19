20e055fc201aa96e9c9e5425823c0637fe9b62a1
Rubberbandits <exodusthefirst@gmail.com>
2014-04-18 17:31:57 -0700

    Update sh_gasmask.lua

--- WIPPlugins/sh_gasmask.lua
+++ WIPPlugins/sh_gasmask.lua
@@ -15,14 +15,15 @@ ITEM.price = 200
 ITEM.functions = {}
 ITEM.functions.Equip = {
 	onRun = function(client)
-	if (client:Health() >= 20) then
-		surface.SetMaterial("effects/gasmask_screen_4.vmt")
-	elseif (client:Health >= 30) then
-		surface.SetMaterial("effects/gasmask_screen_3.vmt")
-	elseif (client:Health >= 60) then
-		surface.SetMaterial("effects/gasmask_screen_2.vmt")
-	elseif (client:Health >= 90) then
-		surface.SetMaterial( "effects/gasmask_screen_1.vmt")
-	else
-		surface.SetMaterial( "effects/gasmask_screen.vmt")
-end
+		if (client:Health() >= 20) then
+			surface.SetMaterial("effects/gasmask_screen_4.vmt")
+		elseif (client:Health >= 30) then
+			surface.SetMaterial("effects/gasmask_screen_3.vmt")
+		elseif (client:Health >= 60) then
+			surface.SetMaterial("effects/gasmask_screen_2.vmt")
+		elseif (client:Health >= 90) then
+			surface.SetMaterial( "effects/gasmask_screen_1.vmt")
+		else
+			surface.SetMaterial( "effects/gasmask_screen.vmt")
+	end
+}
